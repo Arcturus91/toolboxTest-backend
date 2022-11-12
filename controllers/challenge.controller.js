714b4076b55b58a1685b823a8b53f91dab7f22d6
Arcturus91 <arturo.barrantesv@pucp.pe>
2022-11-11 22:33:26 -0500

    req.query implemented

--- controllers/challenge.controller.js
+++ controllers/challenge.controller.js
@@ -9,59 +9,61 @@ const headers = {
 
 //
 exports.getFiles = async (req, res, next) => {
-  try {
-    const { data } = await axios.get(
-      "https://echo-serv.tbxnet.com/v1/secret/files",
-      headers
-    );
-    const titlesArray = data.files;
+  const { fileName } = req.query;
+  if (fileName) {
+    try {
+      const { data } = await axios.get(
+        `https://echo-serv.tbxnet.com/v1/secret/file/${fileName}`,
+        headers
+      );
+      csvData = data;
+      let jsonArray = csvToJson(csvData);
+      let apiResponse = {};
+      apiResponse["file"] = `${fileName}`;
+      apiResponse["lines"] = jsonArray;
+      res.status(200).json(apiResponse);
+    } catch (err) {
+      res.status(err.response.status).json(err.response.statusText);
+    }
+  } else {
+    try {
+      const { data } = await axios.get(
+        "https://echo-serv.tbxnet.com/v1/secret/files",
+        headers
+      );
+      const titlesArray = data.files;
 
-    const filesDetailArray = await Promise.all(
-      titlesArray.map(async (titles) => {
-        try {
-          const { data } = await axios.get(
-            `https://echo-serv.tbxnet.com/v1/secret/file/${titles}`,
-            headers
-          );
-          csvData = data;
-          let jsonArray = csvToJson(csvData);
-          if (jsonArray.length > 0) {
-            let apiResponse = {};
-            apiResponse["file"] = `${titles}`;
-            apiResponse["lines"] = jsonArray;
-            return apiResponse;
+      const filesDetailArray = await Promise.all(
+        titlesArray.map(async (titles) => {
+          try {
+            const { data } = await axios.get(
+              `https://echo-serv.tbxnet.com/v1/secret/file/${titles}`,
+              headers
+            );
+            csvData = data;
+            let jsonArray = csvToJson(csvData);
+            if (jsonArray.length > 0) {
+              let apiResponse = {};
+              apiResponse["file"] = `${titles}`;
+              apiResponse["lines"] = jsonArray;
+              return apiResponse;
+            }
+          } catch (err) {
+            console.log("iteration axios error: ", err);
           }
-        } catch (err) {
-          console.log("iteration axios error: ", err);
-        }
-      })
-    );
+        })
+      );
 
-    const filesFormatted = filesDetailArray.filter((item) => {
-      return item !== undefined;
-    });
+      const filesFormatted = filesDetailArray.filter((item) => {
+        return item !== undefined;
+      });
 
-    res.status(200).json(filesFormatted);
-    console.log(filesFormatted);
-  } catch (err) {
-    console.log("files request error: ", err);
+      res.status(200).json(filesFormatted);
+      console.log(filesFormatted);
+    } catch (err) {
+      console.log("files request error: ", err);
+    }
   }
 };
 
-exports.getData = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const { data } = await axios.get(
-      `https://echo-serv.tbxnet.com/v1/secret/file/${id}.csv`,
-      headers
-    );
-    csvData = data;
-    let jsonArray = csvToJson(csvData);
-    let apiResponse = {};
-    apiResponse["file"] = `${id}.csv`;
-    apiResponse["lines"] = jsonArray;
-    res.status(200).json(apiResponse);
-  } catch (err) {
-    res.status(err.response.status).json(err.response.statusText);
-  }
-};
+
--- routes/challenge.routes.js
+++ routes/challenge.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
-const { getFiles, getData } = require("../controllers/challenge.controller");
+const { getFiles} = require("../controllers/challenge.controller");
 
 router.get("/data", getFiles);
-router.get("/:id", getData);
+
 
 module.exports = router;
+
+
